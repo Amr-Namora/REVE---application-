1837528251b86fd57c8101ffc0f80dd875b42068
amrsy186200414 <amrsy186200414@gmail.com>
2025-10-28 22:04:47 +0300

    Initial commit with secure configuration

--- project/settings.py
+++ project/settings.py
@@ -1,16 +1,172 @@
 
-#######################
+# #######################
+# from pathlib import Path
+# from datetime import timedelta
+# import os
+# from decouple import config
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# AUTH_USER_MODEL = 'account.CustomUser'
+
+# SECRET_KEY = config("SECRET_KEY")
+# DEBUG = True
+# ALLOWED_HOSTS = ['127.0.0.1','192.168.1.107','revesyria.com','www.revesyria.com']
+
+# INSTALLED_APPS = [
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#     'account',
+#     'reservations',
+#     'django_filters',
+#     'rest_framework',
+#     'rest_framework_simplejwt.token_blacklist',
+#     'corsheaders',
+# ]
+
+# MIDDLEWARE = [
+#     'corsheaders.middleware.CorsMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.security.SecurityMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
+
+# # CORS Settings
+# CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'https://www.revesyria.com',
+#     'https://revesyria.com',
+#     'http://127.0.0.1:8000',
+#     'http://127.0.0.1:8081',
+#     'http://192.168.1.107:8000',
+#     'http://192.168.1.107:8081',
+#     'http://localhost:8081',  # Add this line
+
+# ]
+# CSRF_TRUSTED_ORIGINS = [
+
+#    'https://www.revesyria.com',
+#     'https://revesyria.com',
+#     'http://127.0.0.1:8000',
+#     'http://127.0.0.1:8081',
+#     'http://192.168.1.107:8000',
+#     'http://192.168.1.107:8081',
+#     'http://localhost:8081',  # Add this line
+
+# ]
+# ROOT_URLCONF = 'project.urls'
+
+
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [os.path.join(BASE_DIR,'templates')],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+#         'LOCATION': 'unique-cache-name',
+#     }
+# }
+
+
+# WSGI_APPLICATION = 'project.wsgi.application'
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+# # REST Framework Settings
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ),
+#     'DEFAULT_RENDERER_CLASSES': (
+#         'rest_framework.renderers.JSONRenderer',
+#     ),
+# }
+# SIMPLE_JWT = {
+#     'ACCESS_TOKEN_LIFETIME': timedelta(days=365),
+#     'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
+#     'ROTATE_REFRESH_TOKENS': True,
+#     'BLACKLIST_AFTER_ROTATION': True,
+#     'AUTH_HEADER_TYPES': ('Bearer',),
+#     'USER_ID_FIELD': 'id',
+#     'USER_ID_CLAIM': 'user_id',
+# }
+
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
+
+# LANGUAGE_CODE = 'en-us'
+# TIME_ZONE = 'UTC'
+# USE_I18N = True
+# USE_TZ = True
+# DEFAULT_CHARSET = 'utf-8'
+# # STATIC_ROOT='staticfiles'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS=[
+# os.path.join(BASE_DIR,'project/static')
+# ]
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# AUTHENTICATION_BACKENDS = [
+#     'django.contrib.auth.backends.ModelBackend',
+# ]
+
+# in the server
+#############################
+
+
 from pathlib import Path
 from datetime import timedelta
 import os
-from decouple import config
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 AUTH_USER_MODEL = 'account.CustomUser'
 
-SECRET_KEY = config("SECRET_KEY")
-DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1','revesyria.com','www.revesyria.com']
+load_dotenv() 
+
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"  # Better practice
+ALLOWED_HOSTS = ['revesyria.com', 'www.revesyria.com']
+# ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -44,22 +200,19 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'https://www.revesyria.com',
     'https://revesyria.com',
-    'http://127.0.0.1:8000'
 
 ]
 CSRF_TRUSTED_ORIGINS = [
 
     'https://revesyria.com',
     'https://www.revesyria.com',
-    'http://127.0.0.1:8000/'
 ]
 ROOT_URLCONF = 'project.urls'
-
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,22 +225,27 @@ TEMPLATES = [
     },
 ]
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': 'unique-cache-name',
-    }
-}
-
-
 WSGI_APPLICATION = 'project.wsgi.application'
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+#         'LOCATION': 'redis://localhost:6379/1',
+#     }
+# }
+
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+
     }
 }
-
 
 # REST Framework Settings
 REST_FRAMEWORK = {
@@ -131,8 +289,8 @@ DEFAULT_CHARSET = 'utf-8'
 # STATIC_ROOT='staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-os.path.join(BASE_DIR,'project/static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'project/static')
 ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
@@ -141,155 +299,5 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-# in the server
-#############################
 
-
-# from pathlib import Path
-# from datetime import timedelta
-# import os
-# from decouple import config
-# from dotenv import load_dotenv
-#
-# BASE_DIR = Path(__file__).resolve().parent.parent
-# AUTH_USER_MODEL = 'account.CustomUser'
-#
-# load_dotenv(os.path.join(os.path.dirname(__file__), "../.env"))
-#
-# DEBUG = False
-# ALLOWED_HOSTS = ['revesyria.com', 'www.revesyria.com']
-# # ALLOWED_HOSTS = ['*']
-#
-# INSTALLED_APPS = [
-#     'django.contrib.admin',
-#     'django.contrib.auth',
-#     'django.contrib.contenttypes',
-#     'django.contrib.sessions',
-#     'django.contrib.messages',
-#     'django.contrib.staticfiles',
-#     'account',
-#     'reservations',
-#     'django_filters',
-#     'rest_framework',
-#     'rest_framework_simplejwt.token_blacklist',
-#     'corsheaders',
-# ]
-#
-# MIDDLEWARE = [
-#     'corsheaders.middleware.CorsMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.security.SecurityMiddleware',
-#     'django.contrib.sessions.middleware.SessionMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'django.contrib.messages.middleware.MessageMiddleware',
-#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-# ]
-#
-# # CORS Settings
-# CORS_ALLOW_CREDENTIALS = True
-# CORS_ALLOWED_ORIGINS = [
-#     'https://www.revesyria.com',
-#     'https://revesyria.com',
-#
-# ]
-# CSRF_TRUSTED_ORIGINS = [
-#
-#     'https://revesyria.com',
-#     'https://www.revesyria.com',
-# ]
-# ROOT_URLCONF = 'project.urls'
-#
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [os.path.join(BASE_DIR, 'templates')],
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
-#
-# WSGI_APPLICATION = 'project.wsgi.application'
-# # CACHES = {
-# #     'default': {
-# #         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-# #         'LOCATION': 'redis://localhost:6379/1',
-# #     }
-# # }
-#
-#
-# SECRET_KEY = os.getenv("SECRET_KEY")
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("DB_NAME"),
-#         "USER": os.getenv("DB_USER"),
-#         "PASSWORD": os.getenv("DB_PASSWORD"),
-#         "HOST": os.getenv("DB_HOST"),
-#         "PORT": os.getenv("DB_PORT"),
-#
-#     }
-# }
-#
-# # REST Framework Settings
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework_simplejwt.authentication.JWTAuthentication',
-#     ),
-#     'DEFAULT_RENDERER_CLASSES': (
-#         'rest_framework.renderers.JSONRenderer',
-#     ),
-# }
-# SIMPLE_JWT = {
-#     'ACCESS_TOKEN_LIFETIME': timedelta(days=365),
-#     'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
-#     'ROTATE_REFRESH_TOKENS': True,
-#     'BLACKLIST_AFTER_ROTATION': True,
-#     'AUTH_HEADER_TYPES': ('Bearer',),
-#     'USER_ID_FIELD': 'id',
-#     'USER_ID_CLAIM': 'user_id',
-# }
-#
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#     },
-# ]
-#
-# LANGUAGE_CODE = 'en-us'
-# TIME_ZONE = 'UTC'
-# USE_I18N = True
-# USE_TZ = True
-# DEFAULT_CHARSET = 'utf-8'
-# # STATIC_ROOT='staticfiles'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'project/static')
-# ]
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# AUTHENTICATION_BACKENDS = [
-#     'django.contrib.auth.backends.ModelBackend',
-# ]
-#
-#
-#############
+############
